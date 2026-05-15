97b52ba645655b1491dd212e0d7eb8c65982a21c
Antoine <antoine.armand21@gmail.com>
2026-05-15 22:13:45 +0200

    [ADD] Création d'une nouvelle fenêtre de menu pour DropItems.

--- VraiPseudoSae/view/DropItemsMenu.xaml.cs
+++ VraiPseudoSae/view/DropItemsMenu.xaml.cs
@@ -8,17 +8,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace VraiPseudoSae.view
 {
     /// <summary>
-    /// Logique d'interaction pour DropItems.xaml
+    /// Logique d'interaction pour DropItemsMenu.xaml
     /// </summary>
-    public partial class DropItems : Window
+    public partial class DropItemsMenu : Window
     {
-        public DropItems()
+        public DropItemsMenu()
         {
             InitializeComponent();
         }
