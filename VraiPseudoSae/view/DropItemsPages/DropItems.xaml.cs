df6620bd0dce817101e2a30a2171ebd2a56563db
Antoine <antoine.armand21@gmail.com>
2026-06-07 19:29:23 +0200

    [ADD - DROPITEMS] Ajout du troisième décor : l'Aube, et modification du système de limitation.

--- VraiPseudoSae/view/DropItemsPages/DropItems.xaml.cs
+++ VraiPseudoSae/view/DropItemsPages/DropItems.xaml.cs
@@ -43,7 +43,7 @@ namespace VraiPseudoSae.view
                     ConteneurDecor.Content = new DecorJour();
                     break;
                 case DropItemsMapsEnum.AUBE:
-                    // ConteneurDecor.Content = new DecorAube();
+                    ConteneurDecor.Content = new DecorAube();
                     break;
                 default:
                     ConteneurDecor.Content = new DecorNuit(); // Sécurité
